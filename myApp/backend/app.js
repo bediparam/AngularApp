a7cb73e667d9e111414a52a42ecfeb3bec980ea1
parambedi <parambedi24@gmail.com>
2020-03-13 15:12:53 +0530

    fixed deletepost method

--- myApp/backend/app.js
+++ myApp/backend/app.js
@@ -42,7 +42,7 @@ app.post('/api/posts',(req, res, next)=>{
 
 });
 
-app.use('/api/posts',(req, res, next)=>{
+app.get('/api/posts',(req, res, next)=>{
   Post.find().then(documents => {
     res.status(200).json({
       posts:documents,
@@ -51,6 +51,14 @@ app.use('/api/posts',(req, res, next)=>{
   });
 });
 
+app.delete("/api/posts/:id", (req, res, next) =>{
+  Post.deleteOne({_id: req.params.id}).then( result => {
+    console.log(result);
+    res.status(200).json({message:"Post deleted successfully"});
+  })
+
+
+})
 
 
 module.exports = app;
